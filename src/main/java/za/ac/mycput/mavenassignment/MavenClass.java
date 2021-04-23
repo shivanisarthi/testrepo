0603911b0039fd80ccccf320e425db0318d80cef
shivanisarthi <shivani.sarthi@knoldus.com>
2021-04-23 09:48:26 +0530

    adding

---
+++ src/main/java/za/ac/mycput/mavenassignment/MavenClass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.mycput.mavenassignment;
+
+/**
+ * @author Ntobeko Magam
+ * @student Number 216108055
+ */
+public class MavenClass {
+
+    /**
+     * 
+     * program to add and subtract numbers
+     * 
+     */
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    
+}
